16f2f790d5dc56481b5a59e6a7e605a6f1c2056a
d.salvatore <d.salvatore@techgap.it>
2018-02-09 16:50:30 +0100

    Initialization of a Spring Boot Application

---
+++ src/main/java/com/atifovac/sso/SSOApplication.java
@@ -0,0 +1,12 @@
+package com.atifovac.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SSOApplication {
+    public static void main(String... args) {
+        System.out.println("HW!!");
+        SpringApplication.run(SSOApplication.class, args);
+    }
+}
